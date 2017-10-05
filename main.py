87f9f84ca2fd19c4dd4c56a392033f0da9f0844f
MavenHyun <mavenhyun@gmail.com>
2017-10-05 16:24:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import panda
 
-expression_data = "data/GeneExpression/retrospective_breast_rna_seq_sort_common_gene_15115.txt"
-grn_data = "data/GeneRegulatoryNetwork/TRRUST/trrust_rawdata_geneSymbol.human.txt"
-ppi_data = "data/PPI/ppi_biogrid.txt"
-output_file = 'C:/Users/Arthur Keonwoo Kim/Desktop/PyPanda/output/result_breast_rna.tsv'
+expression_data = "./retrospective_breast_rna_seq_sort_common_gene_15115.txt"
+grn_data = "./trrust_rawdata_geneSymbol_duplicateEliminated.human.txt"
+ppi_data = "./ppi_biogrid.txt"
+output_file = './result_breast_rna.tsv'
 
-p = panda.Panda(expression_data, grn_data, ppi_data, remove_missing=False)
+p = panda.Panda(expression_data, grn_data, ppi_data, remove_missing=True)
 p.save_panda_results(file=output_file)
 
